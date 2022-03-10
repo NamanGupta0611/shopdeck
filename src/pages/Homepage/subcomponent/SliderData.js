1e84c9a39d9a8b1357c6463d91abf5bf2faec0b3
Naman Gupta <namangupta.gupta62@gmail.com>
2022-03-10 14:55:11 +0530

    Naman Gupta

---
+++ src/pages/Homepage/subcomponent/SliderData.js
@@ -0,0 +1,15 @@
+
+export const SliderData = [
+    {
+        image: 'https://m.media-amazon.com/images/I/61hxRZwuIDL._SX3000_.jpg'
+    },
+    {
+        image: 'https://m.media-amazon.com/images/I/71E+KZ9vagL._SX3000_.jpg'
+    },
+    {
+        image: './deals.png'
+    },
+    {
+        image: 'https://blog.dupontregistry.com/wp-content/uploads/2018/07/Pagani-Feature.jpg'
+    },
+]
