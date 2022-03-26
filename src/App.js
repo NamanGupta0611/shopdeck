d3392497a4fcbe25744f8ff93c2e6639b1c42db0
Naman Gupta <namangupta.gupta62@gmail.com>
2022-03-26 10:03:02 +0530

    Naman

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
-import Payment from './pages/Payment/payment';
 import Cart from './pages/cart/cart';
-import Home from './pages/Homepage/Home';
 import Details from './pages/Details/Details';
 
 // const StyledButton = styled-Component
@@ -16,8 +14,6 @@ function App() {
     <div> 
       <Router>
         <Routes>
-           <Route path="/"  element={<Home />} />
-           <Route path="/payment"  element={<Payment />} />
            <Route path="/login"  element={<Login />} />
            <Route path= "/signup"  element= {<Signup />} />
            <Route path="/cart" element={<Cart />} />
